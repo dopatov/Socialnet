1763f678488c916cfda12610c86284ef01e9c412
Daniel <Alvarez.v.d@gmail.com>
2016-07-18 12:15:47 +0200

    act 18/07

--- src/main/java/com/example/domain/Persona.java
+++ src/main/java/com/example/domain/Persona.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public clasPersona {
+public class Persona {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
